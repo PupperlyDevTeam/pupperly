118a1e186417672d727bfacc887c3020d9aaa8c0
Nick <nandreala@gmail.com>
2021-11-01 22:11:16 -0400

    removed dev testing data

--- pages/petprofile.tsx
+++ pages/petprofile.tsx
@@ -61,8 +61,7 @@ const PetProfile: NextPage = () => {
           fetch('/.netlify/functions/getPetProfile', {
             method: 'POST',
             body: JSON.stringify({
-              _id: '65465469849615',
-              // _id: '13035135',
+              _id: '13035135',
             }),
           })
             .then((res) => res.json())
@@ -92,37 +91,17 @@ const PetProfile: NextPage = () => {
           fetch('/.netlify/functions/updatePetProfile', {
             method: 'POST',
             body: JSON.stringify({
-              _id: '65465469849615',
-              allergies: JSON.stringify(['mold', 'wheat', 'beef']),
-              breed: 'dog',
-              dob: null,
-              med_hx: JSON.stringify([
-                '{name: urinary tract infection, date: 06/2019}',
-              ]),
-              medications: JSON.stringify([
-                '{name:Sentinel, frequency: 1 tab every month},{name:Fluoxetine, frequency: 1 0.75mg capsule q12 PRN} ',
-              ]),
-              name: 'Ghost',
-              sex: 'Female',
-              species: 'Husky/Shepard',
-              surg_hx: JSON.stringify([
-                '{surgery: spay, date: 10/2017}, {surgery: foreign body removed from LF paw, date: 05/2019}',
-              ]),
-              vaccinations: JSON.stringify([
-                '{name: Rabies, date: 12/2020, due: 12/2024}, {name: Influenza, date: 12/2020, due: 12/2021}',
-              ]),
-
-              // _id: '26236',
-              // allergies: 'water',
-              // breed: 'Koi',
-              // dob: '2020-12-12',
-              // med_hx: JSON.stringify(['debloating']),
-              // medications: JSON.stringify(['fish zoloft']),
-              // name: 'Goldfish',
-              // sex: 'M',
-              // species: 'Fish',
-              // surg_hx: null, //must json.stringify any array values
-              // vaccinations: null //must json.stringify any array values
+              _id: '26236',
+              allergies: 'water',
+              breed: 'Koi',
+              dob: '2020-12-12',
+              med_hx: JSON.stringify(['debloating']),
+              medications: JSON.stringify(['fish zoloft']),
+              name: 'Goldfish',
+              sex: 'M',
+              species: 'Fish',
+              surg_hx: null, //must json.stringify any array values
+              vaccinations: null, //must json.stringify any array values
             }),
           })
             .then((res) => res.json())
